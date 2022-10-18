cef7bd03db09fc81f33245aa5eda39ce27392d56
Renato Costa <renatoaccosta@gmail.com>
2022-10-18 15:03:56 +0100

    finish miniserver

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/", function (req, res) {
+  let date = new Date();
+  res.json({ unix: date.valueOf(), utc: date.toUTCString() });
 });
 
 app.get("/api/:date", (req, res) => {
@@ -29,8 +30,9 @@ app.get("/api/:date", (req, res) => {
   let dateObject = new Date(dateString);
 
   if (dateObject.toUTCString() === "Invalid Date") {
-    res.json({ error: "Invaid Date" });
+    res.json({ error: dateObject.toUTCString() });
   }
+
   if (/^\d+$/.test(dateString)) {
     dateInt = parseInt(dateString);
 
